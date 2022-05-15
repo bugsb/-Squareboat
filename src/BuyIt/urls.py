c41eb190e7e95f385166975442b9742aa8f65118
mani <manishi.anand@intimetec.com>
2022-05-15 22:46:54 +0530

    debug

--- src/BuyIt/urls.py
+++ src/BuyIt/urls.py
@@ -7,7 +7,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/',include('api.urls')),
-    path('app/',include('app.urls')),
+    path('',include('app.urls')),
     path('auth/',include('login.urls')),
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
